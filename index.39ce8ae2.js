682f3d28251de96a165e821dfc14320b8acdf1b8
Oleksandr Marchenko <sasha@mate.academy>
2024-03-05 17:00:40 +0200

    make build

---
+++ index.39ce8ae2.js
@@ -0,0 +1,2 @@
+window.addEventListener("hashchange",function(){"#menu"===window.location.hash?document.body.classList.add("page__body--with-menu"):document.body.classList.remove("page__body--with-menu")}),document.querySelector(".contact-us__form").addEventListener("submit",function(e){e.preventDefault(),e.target.reset()});
+//# sourceMappingURL=index.39ce8ae2.js.map
